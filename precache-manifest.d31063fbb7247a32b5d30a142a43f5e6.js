c898ef57a867686105d1d9a985eb4bb043ac9b24
jack5661 <34701860+jack5661@users.noreply.github.com>
2020-05-21 13:14:11 -0700

    Updates

---
+++ precache-manifest.d31063fbb7247a32b5d30a142a43f5e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de4e3cca1be693cd0ab52dca6193444",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "0b5c4997e47e5644f24a",
+    "url": "/react-restaurant/static/css/main.d958e8c3.chunk.css"
+  },
+  {
+    "revision": "ad744e00888729c38d6d",
+    "url": "/react-restaurant/static/js/2.e76a0a4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-restaurant/static/js/2.e76a0a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5c4997e47e5644f24a",
+    "url": "/react-restaurant/static/js/main.15226a77.chunk.js"
+  },
+  {
+    "revision": "8554597e19aae3ec1e54",
+    "url": "/react-restaurant/static/js/runtime-main.0ba26c4f.js"
+  },
+  {
+    "revision": "910b2d91c9dbe719faaf1af3c7e1b612",
+    "url": "/react-restaurant/static/media/buffet.910b2d91.jpeg"
+  },
+  {
+    "revision": "316fa5ffc2a87c46cbc0073425c4d6b4",
+    "url": "/react-restaurant/static/media/burgers.316fa5ff.jpeg"
+  },
+  {
+    "revision": "6bab57e0ace1e8673c4ed2bc4f14a639",
+    "url": "/react-restaurant/static/media/burrito.6bab57e0.jpeg"
+  },
+  {
+    "revision": "21c4413ca3cfc331ece6747767f6958a",
+    "url": "/react-restaurant/static/media/cake.21c4413c.jpeg"
+  },
+  {
+    "revision": "0ea24a1003945133e89392e472bcf306",
+    "url": "/react-restaurant/static/media/fruits.0ea24a10.jpeg"
+  },
+  {
+    "revision": "9c047a0fbd40286c279483f9aeef7c71",
+    "url": "/react-restaurant/static/media/icecream.9c047a0f.jpg"
+  },
+  {
+    "revision": "8b929915439a8d1ce388da6f91488118",
+    "url": "/react-restaurant/static/media/pancakes.8b929915.jpeg"
+  },
+  {
+    "revision": "dfb65c861f2c7a9411b8ef22e4faaa45",
+    "url": "/react-restaurant/static/media/restaurant.dfb65c86.jpeg"
+  }
+]);
